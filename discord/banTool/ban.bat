6339dc200f62e624aed5afd53beded7611ec719b
Shunshun94 <shunshun9460@gmail.com>
2023-03-01 23:21:28 +0900

    Update ban.bat

--- discord/banTool/ban.bat
+++ discord/banTool/ban.bat
@@ -1,1 +1,1 @@
-curl -X PUT -H "Authorization:Bot %0" -d "{}" https://discordapp.com/api/guilds/%1/bans/%2
+curl -X PUT -H "Authorization:Bot %1" -d "{}" https://discordapp.com/api/guilds/%2/bans/%3
